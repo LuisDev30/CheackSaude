941c0b515c10911f347f99409ccb3e4a8201d635
Luisdev30 <luis.dev025@gmaiil.com>
2023-04-18 19:48:46 -0300

    Atualização

--- app/src/main/java/com/example/empregaeu4/MainActivity.kt
+++ app/src/main/java/com/example/empregaeu4/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.empregaeu4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import com.example.empregaeu4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val edbutton = binding.idButton
+
+        edbutton.setOnClickListener{
+            val intent = Intent(this,segunda_tela::class.java)
+            startActivity(intent)
+        }
+
+        val adbutton = binding.idcadastro
+
+        adbutton.setOnClickListener{
+            val intent = Intent(this,terceira_tela::class.java)
+            startActivity(intent)
+        }
     }
 }
